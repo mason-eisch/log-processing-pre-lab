4f5f83583cfd0a41faffcbcae3458eb17d39e787
mason-eisch <eisch115@morris.umn.edu>
2022-09-07 20:33:25 -0500

    quit bothering with declaring variables; wrote one line to concatenate all of the stuff :D

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -1,9 +1,12 @@
 #! /usr/bin/bash
 
 # Declare key variables using arguments
-header = "${2}_header.html"
-footer = "${2}_footer.html"
+#header = ${2}_header.html
+#footer = ${2}_footer.html
+#content = $1
 
+#echo $header
 
 # Concatenate contents into target files
-(cat $header $1 $footer) >>$3
+#cat $header $content $footer >$3
+cat ${2}_header.html $1 ${2}_footer.html >$3

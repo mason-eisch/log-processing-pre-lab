4c73b1e3a80d72dfd711b2f7063c2c343ce0c7f4
mason-eisch <eisch115@morris.umn.edu>
2022-09-07 20:35:00 -0500

    cleaned up script/improved comments

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -1,12 +1,4 @@
 #! /usr/bin/bash
 
-# Declare key variables using arguments
-#header = ${2}_header.html
-#footer = ${2}_footer.html
-#content = $1
-
-#echo $header
-
-# Concatenate contents into target files
-#cat $header $content $footer >$3
+# Concatenate header/content/footer into target file
 cat ${2}_header.html $1 ${2}_footer.html >$3

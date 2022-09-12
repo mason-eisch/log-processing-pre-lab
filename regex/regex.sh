cc5c752f943b12fcd803080bea11ba04187bbab7
mason-eisch <eisch115@morris.umn.edu>
2022-09-12 18:02:08 -0500

    added line that extracts sandwich orders from r2_input.txt

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,3 +1,4 @@
 #! usr/bin/bash
 awk 'match($0, /(\w+), (\w+)/, info) {print "1. " info[1] "\n" "2. " info[2] "\n" }' < r0_input.txt
 awk 'match($0, /([A-Z]\w+).+is ([hat]\w+)/, sandwich) {print "1. " sandwich[1] "\n" "2. " sandwich[2] "\n"}' < r1_input.txt
+awk 'match($0, /th (\S+) (.+)/, orders) {print "1. " orders[1] "\n" "2. " orders[2] "\n"}' r2_input.txt
